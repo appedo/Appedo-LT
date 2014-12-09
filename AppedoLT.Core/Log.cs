95b2cc84d8832c16a826bee75e3b12b90b316253
rasiths <rasiths@softsmith.com>
2014-12-09 18:21:20 +0530

    Change test

--- AppedoLT.Core/Log.cs
+++ AppedoLT.Core/Log.cs
@@ -5,8 +5,7 @@ using System.Text;
 
 namespace AppedoLT.Core
 {
-   public class Log
-
+    public class Log
     {
         public string loadGen = Constants.GetInstance().LoadGen;
         public string reportname = string.Empty;
@@ -19,12 +18,11 @@ namespace AppedoLT.Core
         public string iterationid = string.Empty;
         public string message = string.Empty;
         public DateTime time = new DateTime();
-        
+
         public override string ToString()
         {
-
             return string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},\"{9}\",{10}", this.loadGen, this.reportname, this.scenarioname, this.scriptid, this.scriptname,
-                                                                                     this.logid,this.logname,this.userid, this.iterationid, this.message.Replace("\"", "\"\""), this.time.ToString("yyyy-MM-dd HH:mm:ss"));
+                                                                                     this.logid, this.logname, this.userid, this.iterationid, this.message.Replace("\"", "\"\""), this.time.ToString("yyyy-MM-dd HH:mm:ss"));
         }
     }
 }
