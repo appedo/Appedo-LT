0ecd55d1f525fc2590e0c0ae3c2ede5ce3ea68cb
rasiths <rasiths@softsmith.com>
2015-01-07 11:43:23 +0530

    test1

--- AppedoLT.Core/Connection.cs
+++ AppedoLT.Core/Connection.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Configuration;
 using System.Security.Authentication;
 using System.Text.RegularExpressions;
+
 namespace AppedoLT.Core
 {
     public class Connection
